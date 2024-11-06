e1b6d298b291d8845b74813157c160e595148ca4
kix300 <kixwalkiki@gmail.com>
2024-11-06 15:56:32 +0100

    vsplit terminal

--- config/lua/config/keymaps.lua
+++ config/lua/config/keymaps.lua
@@ -3,8 +3,7 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<C-t>", function()
-	vim.cmd("tabnew | terminal")
-	vim.cmd("tabmove +1") 
+	vim.cmd("vsplit | terminal")
 end, { desc = "Ouvrir un terminal dans un nouvel onglet" })
 
 vim.keymap.set("n", "<A-t>", ":bdelete!<CR>", { desc = "Fermer l'onglet actuel" })
